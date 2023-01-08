b19a99b0901bda94c11ccaed89dcb07f2cd0618c
Lucas Boucher <contact@lucasboucher.fr>
2023-01-08 15:58:32 +0100

    fix

--- scrapper/main.py
+++ scrapper/main.py
@@ -12,7 +12,7 @@ import re
 print(datetime.now().strftime('%H:%M:%S'), '- Recovery of data in progress...')
 
 # Initialisation de la page
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15'}
 page = requests.get(championship_url, headers=headers)
 soup = BeautifulSoup(page.text, 'html.parser')
 print(page)
