587b2d68bfde56ec706f2aecd469edd1098cc4ab
Lucas Boucher <contact@lucasboucher.fr>
2024-01-10 17:35:13 +0100

    FIX: path api

--- server/api.py
+++ server/api.py
@@ -2,14 +2,19 @@ from flask import Flask, make_response
 from flask_cors import CORS
 from subprocess import run, CalledProcessError
 import json
+import os
 
 api = Flask(__name__)
 CORS(api)
 
+server_directory = os.path.abspath(os.path.dirname(__file__))
+data_path = os.path.join(server_directory, 'data.json')
+scraper_path = os.path.join(server_directory, 'scraper.py')
+
 @api.route('/data', methods=['GET'])
 def data():
     try:
-        with open('data.json', 'r') as file:
+        with open(data_path, 'r') as file:
             data = json.load(file)
         response = make_response(data, 200)
         return response
@@ -20,7 +25,7 @@ def data():
 @api.route('/scrape', methods=['GET'])
 def scrape():
     try:
-        run(['python3', 'scraper.py'], check=True)
+        run(['python3', scraper_path], check=True)
         response = make_response('OK', 200)
         return response
     except CalledProcessError:
