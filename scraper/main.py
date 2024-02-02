c51a7458ed6b3d017412ef67fe6f316d1bee64df
Lucas Boucher <contact@lucasboucher.fr>
2024-02-02 16:52:04 +0100

    UPDATE: error handler ConnectionError

--- scraper/main.py
+++ scraper/main.py
@@ -4,6 +4,7 @@ def run_scraper():
     from datetime import datetime
     from bs4 import BeautifulSoup
     from dotenv import load_dotenv, find_dotenv
+    from requests.exceptions import ConnectionError
     import requests
     import json
     import re
@@ -25,7 +26,7 @@ def run_scraper():
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15'}
     try:
         page = requests.get(CHAMPIONSHIP_URL, headers=headers, proxies=proxy)
-    except ConnectionRefusedError:
+    except ConnectionError:
         print("Les données ont bien été générées")
     print(datetime.now().strftime('%H:%M:%S'), '- Page response : ', page)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -57,7 +58,7 @@ def run_scraper():
     ranking_url = 'https://resultats.ffbb.com/championnat/classements/' + championship_id + POOL_ID + '.html'
     try:
         page = requests.get(ranking_url, headers=headers, proxies=proxy)
-    except ConnectionRefusedError:
+    except ConnectionError:
         print("Les données ont bien été générées")
     soup = BeautifulSoup(page.text, 'html.parser')
     teams = soup.select('.liste tr')
@@ -86,7 +87,7 @@ def run_scraper():
             fixtures_url = 'https://resultats.ffbb.com/championnat/equipe/division/' + championship_id + POOL_ID + club_id + '.html'
             try:
                 page = requests.get(fixtures_url, headers=headers, proxies=proxy)
-            except ConnectionRefusedError:
+            except ConnectionError:
                 print("Les données ont bien été générées")
             soup = BeautifulSoup(page.text, "html.parser")
             fixtures = soup.select(".liste tr")
